8a3372b7e3d9c0e85dee1ec519d71ff220192071
pritam124 <pritamshah378@gmail.com>
2017-04-09 23:32:21 +0530

    First commit

---
+++ stockbasicbot/Controllers/MessagesController.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+
+namespace stockbasicbot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                int length = (activity.Text ?? string.Empty).Length;
+                //await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                string StockRateString = await GetStock(activity.Text);
+                Activity reply = activity.CreateReply(StockRateString);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+                // return our reply to the user  
+                //return message.CreateReplyMessage(StockRateString);  
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<string> GetStock(string StockSymbol) {
+            double? dblStockValue = await Yahoobot.GetStockRateAsync(StockSymbol);
+            if (dblStockValue == null)
+            {
+                return string.Format("This \"{0}\" is not an valid stock symbol", StockSymbol);
+            }
+            else {
+                return string.Format("Stock :{0}\n Price :{1} ", StockSymbol, dblStockValue);
+            }
+        }
+
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ stockbasicbot/Yahoobot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace stockbasicbot
+{
+    public class Yahoobot
+    {
+        public static async Task<double?> GetStockRateAsync(string StockSymbol)
+        {
+            try {
+                string ServiceURL = $"http://finance.yahoo.com/d/quotes.csv?s={StockSymbol}&f=sl1d1nd";
+                string ResultInCSV;
+                using (WebClient client = new WebClient())
+                {
+                    ResultInCSV = await client.DownloadStringTaskAsync(ServiceURL).ConfigureAwait(false);
+                }
+                var FirstLine = ResultInCSV.Split('\n')[0];
+                var price = FirstLine.Split(',')[1];
+                if (price != null && price.Length >= 0) {
+                    double result;
+                    if (double.TryParse(price, out result)) {
+                        return result;
+                    }
+                }
+                return null;
+            }
+            catch (WebException ex) {
+                throw ex;
+            }
+        }
+    }
+}
